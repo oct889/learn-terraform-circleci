3ad3545b4703469db19c3e6ec92c067f4c124561
oct889 <contact@nicholaswinter.com>
2022-09-06 21:03:12 -0500

    associated key pair to instance

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name = aws_key_pair.deployer
+  key_name = "deployer-key"
 
   tags = {
     Name = "HelloWorld"
