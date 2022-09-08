cf6bd00b8c53a56500d6318102e2bd39ddf40f60
oct889 <contact@nicholaswinter.com>
2022-09-07 22:07:43 -0500

    output private ip and no more s3

--- main.tf
+++ main.tf
@@ -10,41 +10,6 @@ provider "aws" {
 
 resource "random_uuid" "randomid" {}
 
-resource "aws_s3_bucket" "app" {
-  tags = {
-    Name          = "App Bucket"
-    public_bucket = true
-  }
-
-  bucket        = "${var.app}.${var.label}.${random_uuid.randomid.result}"
-  force_destroy = true
-}
-
-resource "aws_s3_object" "app" {
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.app.id
-  content      = file("./assets/index.html")
-  content_type = "text/html"
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_website_configuration" "terramino" {
-  bucket = aws_s3_bucket.app.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
 resource "aws_vpc" "prod_vpc" {
   cidr_block = "10.0.0.0/16"
 
