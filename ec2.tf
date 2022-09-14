50144f993a1190e47f5a4be3338eb278dc52d1d1
oct889 <contact@nicholaswinter.com>
2022-09-13 22:09:36 -0500

    sec groups

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,26 @@
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.prod_vpc.id
+# resource "aws_security_group" "allow_all" {
+#   name        = "allow_all"
+#   description = "Allow all inbound traffic"
+#   vpc_id      = aws_vpc.prod_vpc.id
 
-  ingress {
-    description      = "allow all from internet"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   ingress {
+#     description      = "allow all from internet"
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = {
-    Name = "allow_all"
-  }
-}
+#   tags = {
+#     Name = "allow_all"
+#   }
+# }
