ddb32b5af16e07c75b9adb150a558d4ba52f7c3c
oct889 <contact@nicholaswinter.com>
2022-09-13 22:29:25 -0500

    sec groups

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name = "deployer-key"
   subnet_id = "${aws_subnet.public1a.id}"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
