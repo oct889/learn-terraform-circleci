8a8c9c2918f2d8b33b271b4ebd4f1a1054d4e7fd
oct889 <contact@nicholaswinter.com>
2022-09-14 20:45:01 -0500

    output public ip and give variable name tag

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
 variable "region" {}
 variable "label" {}
 variable "app" {}
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type = string
+  default = "ExampleAppServerInstance"
+}
