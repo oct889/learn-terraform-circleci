8269edbb8cbbeaf4bb12127ceeaa75c502f88518
Nick <nick@Ditis-MacBook-Pro.local>
2022-09-29 15:24:34 +0530

    cleaned things up

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "private_ips" {
+output "bastion_private_ips" {
   value = aws_instance.bastion_host.private_ip
 }
 
-output "public_ips" {
+output "bastion_public_ips" {
   value = aws_instance.web.public_ip
 }
 
-output "private_ips" {
+output "server_private_ips" {
   value = aws_instance.private_host.private_ip
 }
 
-output "public_ips" {
+output "server_public_ips" {
   value = aws_instance.private_host.public_ip
 }
