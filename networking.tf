31136f7c6e1957bb352bf568814392576c2d96a8
Nick <nick@Ditis-MacBook-Pro.local>
2022-09-30 10:36:19 +0530

    ad stuff

--- networking.tf
+++ networking.tf
@@ -85,3 +85,19 @@ resource "aws_route_table" "server1a_rt" {
     Name = "Server 1a Route Table"
   }
 }
+
+resource "aws_directory_service_directory" "terra_ad" {
+  name     = "corp.notexample.com"
+  password = "SuperSecretPassw0rd"
+  edition  = "Standard"
+  type     = "MicrosoftAD"
+
+  vpc_settings {
+    vpc_id     = aws_vpc.prod_vpc.id
+    subnet_ids = [aws_subnet.server1a.id]
+  }
+
+  tags = {
+    Project = "foo"
+  }
+}
